30ea1d572b4e8cecff0344fe31bf260a2d4fac69
ParkGana <gnchoco97@gmail.com>
2024-01-09 15:22:21 +0900

    style 수정

--- src/page/checkbox/checkbox-page.style.ts
+++ src/page/checkbox/checkbox-page.style.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export namespace CheckboxPageStyle {
     export const Container = styled.div`
         display: grid;
-        grid-template-columns: repeat(4, auto);
+        grid-template-columns: repeat(3, auto);
         gap: 10px 20px;
     `
 }
