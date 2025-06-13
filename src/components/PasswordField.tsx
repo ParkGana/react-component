10526fad2e16976da571bf9b1aa7a450bc1b60e0
ParkGana <gnchoco97@gmail.com>
2025-06-13 16:37:59 +0900

    Password Field

---
+++ src/components/PasswordField.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import ComponentContainer from './layout/ComponentContainer';
+import { FaRegEye, FaRegEyeSlash } from 'react-icons/fa6';
+import clsx from 'clsx';
+
+type PasswordFieldProps = {
+  value: string;
+  placeholder?: string;
+  handleChange: (value: string) => void;
+  handleKeyDown: (value: string) => void;
+};
+
+const PasswordField = ({ value, placeholder, handleChange, handleKeyDown }: PasswordFieldProps) => {
+  const [isVisibleValue, setIsVisibleValue] = useState<boolean>(false);
+
+  /* Enter 키 입력 */
+  const handlePressEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    e.key === 'Enter' && handleKeyDown(value);
+  };
+
+  return (
+    <ComponentContainer label="PASSWORD FIELD">
+      <div className="relative w-full">
+        <input
+          className={clsx(
+            'w-full text-body rounded-md border-2 border-gray-400 p-3 pr-10',
+            'focus:border-gray-900 focus:outline-none'
+          )}
+          type={isVisibleValue ? 'text' : 'password'}
+          value={value}
+          placeholder={placeholder}
+          autoComplete="off"
+          onChange={(e) => handleChange(e.target.value)}
+          onKeyDown={handlePressEnter}
+        />
+        <div
+          className="absolute inset-y-0 right-3 flex items-center cursor-pointer"
+          onClick={() => setIsVisibleValue(!isVisibleValue)}
+        >
+          {isVisibleValue ? <FaRegEyeSlash size={20} /> : <FaRegEye size={20} />}
+        </div>
+      </div>
+    </ComponentContainer>
+  );
+};
+
+export default PasswordField;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 import TextField from '../components/TextField';
+import PasswordField from '../components/PasswordField';
 
 const HomePage = () => {
+  const [passwordField, setPasswordField] = useState<string>('');
   const [textField, setTextField] = useState<string>('');
 
   const handleKeyDown = (value: string) => {
@@ -10,6 +12,13 @@ const HomePage = () => {
 
   return (
     <>
+      <PasswordField
+        value={passwordField}
+        placeholder="비밀번호를 입력해 주세요."
+        handleChange={setPasswordField}
+        handleKeyDown={handleKeyDown}
+      />
+
       <TextField
         value={textField}
         placeholder="값을 입력해 주세요."
